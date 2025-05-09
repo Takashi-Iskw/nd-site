af09b1ba0bf3dc04e17474e8762489f3b17d3bfb
Takashi-Iskw <takashi.ishikawa.24@gmail.com>
2025-05-10 01:14:35 +0900

    next.config.mjsからenvを削除

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,6 @@ const nextConfig = {
     config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
     return config;
   },
-};
-
-module.exports = {
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    REGION: process.env.REGION
-  },
-};
+}
 
 export default nextConfig;

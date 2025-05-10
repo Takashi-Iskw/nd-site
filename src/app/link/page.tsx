cde07f8bccd7afea6b7c65ea9c92d51f640d51f5
Takashi-Iskw <takashi.ishikawa.24@gmail.com>
2025-05-10 16:47:56 +0900

    link/page.tsxの表示(className)を編集

--- src/app/link/page.tsx
+++ src/app/link/page.tsx
@@ -1,8 +1,22 @@
 // src/app/(auth)/link/page.tsx
 import LinkPageClient from '@/components/LinkPageClient/LinkPageClient';
+// import { div } from 'framer-motion/client';
 
 export const dynamic = 'force-dynamic';   // ★ これが有効になる
 
 export default function LinkPage() {
-  return <LinkPageClient />;
+  return (
+  <div className="
+       w-full                /* 画面幅いっぱい */
+       max-w-screen-xl       /* 最大幅を1200pxくらいに */
+       mx-auto               /* 中央寄せ */
+       grid
+       grid-rows-[900px_2fr_20px]
+       min-h-screen
+       p-8 pb-20 gap-16
+       sm:p-20
+     ">
+    <LinkPageClient />
+  </div>
+  );
 }
